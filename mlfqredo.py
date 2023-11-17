222cb5e1de0907f04bb17442619cadf33dc1996c
Hawks <126882395+h4wks123@users.noreply.github.com>
2023-11-17 18:29:41 +0800

    [BAYER][Fixed some bugs for new MLFQ]

---
+++ mlfqredo.py
@@ -0,0 +1,132 @@
+class Process:
+    def __init__(self, name, arrival_time, burst_time, position, level):
+        self.name = name
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.remaining_time = burst_time
+        self.level = level
+        self.position = position
+        self.completion_time = 0
+        self.endtime = 0  # Initialize endtime to 0
+        self.waiting_time = 0  # Initialize waiting time to 0
+
+def FirstQuantum(data, Q1, Q2, Q3):
+    
+    processes = [Process(*item) for item in data]
+    processes.sort(key=lambda x: x.arrival_time)
+    demodataset = processes.copy()
+
+    x = i = 0
+    current_time = min(x.arrival_time for x in processes)
+    
+    while i < 5:
+        if demodataset[i].burst_time < Q1 and demodataset[i].burst_time != 0 and demodataset[i].level == 2:
+            current_time += demodataset[i].burst_time
+            demodataset[i].burst_time -= demodataset[i].burst_time
+            demodataset[i].level -= 1
+            print(demodataset[i].name, demodataset[i].burst_time, current_time, demodataset[i].level)
+
+        else:
+            if demodataset[i].level == 2 and demodataset[i].burst_time != 0:
+                demodataset[i].burst_time -= Q1
+                demodataset[i].level -= 1
+                current_time += Q1
+                print(demodataset[i].name, demodataset[i].burst_time, current_time, demodataset[i].level)
+
+                # Check if the next process exists and if its arrival_time is less than the current_time
+                if i + 1 < 5 and current_time < demodataset[i + 1].arrival_time and demodataset[i].level == 1:
+                    if demodataset[x].burst_time < Q2:
+                        current_time += demodataset[x].burst_time
+                        demodataset[x].burst_time -= demodataset[x].burst_time
+                        demodataset[x].level -= 1
+                        print(demodataset[x].name, demodataset[x].burst_time, current_time, demodataset[x].level)
+                    else:
+                        current_time += Q2
+                        demodataset[x].burst_time -= Q2
+                        demodataset[x].level -= 1
+                        print(demodataset[x].name, demodataset[x].burst_time, current_time, demodataset[x].level)
+                    x += 1
+
+        i += 1
+
+    SecondQuantum(demodataset, current_time, Q2, Q3)
+     
+
+def SecondQuantum(demodataset, current_time, Q2, Q3):
+
+    i = 0
+    
+    while i < 5:
+        if demodataset[i].burst_time < Q2 and demodataset[i].burst_time != 0 and demodataset[i].level == 1:
+            current_time += demodataset[i].burst_time
+            demodataset[i].burst_time -= demodataset[i].burst_time
+            demodataset[i].level -= 1
+            print(demodataset[i].name, demodataset[i].burst_time, current_time, demodataset[i].level)  
+
+        else:
+            if demodataset[i].level == 1 and demodataset[i].burst_time != 0:
+                demodataset[i].burst_time -= Q2
+                demodataset[i].level -= 1
+                current_time += Q2
+                print(demodataset[i].name, demodataset[i].burst_time, current_time, demodataset[i].level)
+
+        i += 1
+
+    ThirdQuantum(demodataset, current_time, Q3)
+
+
+def ThirdQuantum(demodataset, current_time, Q3):
+
+    if  Q3 == 1: #If Q3 is 1 then shortest job first is executed
+        while True:
+            # Find the process with the minimum burst time that is not zero
+            min_burst_time = min((x.burst_time for x in demodataset if x.burst_time != 0), default=-1)
+
+            if min_burst_time == -1:
+                break
+            else:
+                # Find the index of the process with the minimum burst time
+                i = 0
+                while demodataset[i].burst_time != min_burst_time:
+                    i += 1
+
+                # Update current time and process information
+                current_time += demodataset[i].burst_time
+                demodataset[i].burst_time = 0  # Set burst time to zero
+                demodataset[i].level -= 1
+                print(demodataset[i].name, demodataset[i].burst_time, current_time, demodataset[i].level)
+
+
+
+if __name__ == "__main__":
+
+    Q1 = 2
+    Q2 = 3
+    Q3 = 1
+    data = [
+        (1, 3, 4, 0, 2),
+        (2, 5, 9, 0, 2),
+        (3, 8, 4, 0, 2),
+        (4, 0, 7, 0, 2),
+        (5, 12, 6, 0, 2)    
+    ]
+
+    result = FirstQuantum(data, Q1, Q2, Q3)
+
+
+    # data = [
+    #     (1, 3, 4, 0, 2),
+    #     (2, 5, 9, 0, 2),
+    #     (3, 8, 4, 0, 2),
+    #     (4, 0, 7, 0, 2),
+    #     (5, 12, 6, 0, 2)    
+    # ]
+
+    # data = [
+    #     (1, 5, 13, 0, 2),
+    #     (2, 25, 15, 0, 2),
+    #     (3, 15, 25, 0, 2),
+    #     (4, 10, 10, 0, 2),
+    #     (5, 20, 35, 0, 2),
+    #     (6, 13, 5, 0, 2)        
+    # ]
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-def sort_by_arrival(processes):
-    return sorted(processes, key=lambda x: x[1])
-
-def sort_by_priority(processes):
-    return sorted(processes, key=lambda x: (x[2], x[3]))
-
-def sort_by_end(processes):
-    return sorted(processes, key=lambda x: x[4])
-
+def sort_by_arrival(lists):
+    sorted_lists = sorted(lists, key=lambda x: x[1])
+    return sorted_lists
+    
+def sort_by_priority(lists):
+    sorted_lists = sorted(lists, key=lambda x: x[2])
+    sorted_lists = sorted(sorted_lists, key=lambda x: x[3])
+    return sorted_lists
+    
+def sort_by_end(lists):
+    sorted_lists = sorted(lists, key=lambda x: x[4])
+    return sorted_lists
+    
 def display_table(process_list):
     print(f"\n\n{'PID':<11}{'AT':<4}{'BT':<4}{'ET':<4}{'TT':<4}{'WT':<4}")
     print("-" * 30)
@@ -19,13 +23,21 @@ def display_table(process_list):
 def display_metrics(process_list):
     sorted_by_end_time = sort_by_end(process_list)
     
-    total_turnaround_time = sum(p[4] for p in process_list)
-    total_waiting_time = sum(p[5] for p in process_list)
-
-    print("\n\nCPU Utilization: " + str(round(total_burst_time / sorted_by_end_time[-1][3] * 100, 2)) + "%")
-    print("Average Turnaround Time: " + str(round(total_turnaround_time / len(process_list), 2)) + " ms")
-    print("Average Waiting Time: " + str(round(total_waiting_time / len(process_list), 2)) + " ms")
-
+    total_turnaround_time = 0
+    total_waiting_time = 0
+    
+    count = 0
+    
+    for p in process_list:
+        total_turnaround_time += process_list[count][4]
+        total_waiting_time += process_list[count][5]
+        
+        count += 1
+   
+    print("\n\nCPU Utilization: " + str(round(total_burst_time/ sorted_by_end_time[-1][3] * 100, 2)) + "%")
+    print("Average Turnaround Time: " + str(round(total_turnaround_time/count, 2)) + " ms")
+    print("Average Waiting Time: " + str(round(total_waiting_time/count, 2)) + " ms")
+    
 def display_chart(timeline):
     print("\n\nGantt Chart:")
     print("-" * 45)
@@ -37,12 +49,11 @@ def display_chart(timeline):
     for t in timeline:
         print(t[1])
         print("     P" + str(t[0][0]))
-
+        
 print("--- Multilevel Feedback Queue --- \n")
-
-num_processes = int(input("How many processes would you like to compute? "))
-quantum_1 = int(input("Enter the time quantum for the first layer: "))
-quantum_2 = int(input("Enter the time quantum for the second layer: "))
+processes = int(input("How many processes would you like to compute? "))
+quantum_1 = int(input("What time quantum would you like to select for the first layer? "))
+quantum_2 = int(input("What time quantum would you like to select for the second layer? "))
 
 layer_1 = []
 layer_2 = []
@@ -62,9 +73,9 @@ remaining_burst_time = 0
 
 timeline = []
 
-print("Enter the arrival time and burst time separated by spaces.\n")
+print("Enter the arrival time and burst time separated with spaces.\n")
 
-for p in range(num_processes):
+for p in range(processes):
     init_info = input(f"P{p+1}: ")
     
     process_list.append(list(map(int, init_info.split())))
@@ -82,23 +93,25 @@ current_time = sorted_by_arrival_time[0][1]
 remaining_burst_time = total_burst_time
 
 while remaining_burst_time != 0:
-    while sorted_by_arrival_time and sorted_by_arrival_time[0][1] <= current_time:
-        layer_1.append(sorted_by_arrival_time.pop(0))
+    while len(sorted_by_arrival_time) > 0 and sorted_by_arrival_time[0][1] <= current_time:
+        layer_1.append(sorted_by_arrival_time[0])
+        sorted_by_arrival_time.pop(0)
     
-    if layer_1:
+    if len(layer_1) > 0:
         if layer_1[0][3] == 1:
             if layer_1[0][2] <= quantum_1:
                 current_time += layer_1[0][2]
                 remaining_burst_time -= layer_1[0][2]
                 
-                final_list[layer_1[0][0] - 1].append(current_time)
+                final_list[layer_1[0][0]-1].append(current_time)
                 
                 layer_1.pop(0)
             else:
                 current_time += quantum_1
                 
-                while sorted_by_arrival_time and sorted_by_arrival_time[0][1] <= current_time:
-                    layer_1.append(sorted_by_arrival_time.pop(0))
+                while len(sorted_by_arrival_time) > 0 and sorted_by_arrival_time[0][1] <= current_time:
+                    layer_1.append(sorted_by_arrival_time[0])
+                    sorted_by_arrival_time.pop(0)
                 
                 layer_1[0][2] -= quantum_1
                 layer_1[0][3] += 1
@@ -106,41 +119,42 @@ while remaining_burst_time != 0:
                 
                 remaining_burst_time -= quantum_1
     else:
-        if layer_2:
+        if len(layer_2) > 0:
             if layer_2[0][2] <= quantum_2:
                 current_time += layer_2[0][2]
                 remaining_burst_time -= layer_2[0][2]
                 
-                final_list[layer_2[0][0] - 1].append(current_time)
+                final_list[layer_2[0][0]-1].append(current_time)
                 
                 layer_2.pop(0)
             else:
                 current_time += quantum_2
                 
-                while sorted_by_arrival_time and sorted_by_arrival_time[0][1] <= current_time:
-                    layer_2.append(sorted_by_arrival_time.pop(0))
+                while len(sorted_by_arrival_time) > 0 and sorted_by_arrival_time[0][1] <= current_time:
+                    layer_2.append(sorted_by_arrival_time[0])
+                    sorted_by_arrival_time.pop(0)
                 
                 layer_2[0][2] -= quantum_2
                 layer_2[0][3] += 1
                 layer_3.append(layer_2.pop(0))
                 
                 remaining_burst_time -= quantum_2
-        elif layer_3:
+        elif len(layer_3) > 0:
             layer_3 = sort_by_arrival(layer_3)
             
             current_time += layer_3[0][2]
             remaining_burst_time -= layer_3[0][2]
             
-            final_list[layer_3[0][0] - 1].append(current_time)
+            final_list[layer_3[0][0]-1].append(current_time)
             
             layer_3.pop(0)
         else:
             current_time += 1
     print(current_time)
-
-for p in range(num_processes):
+        
+for p in range(processes):
     final_list[p].append(final_list[p][3] - final_list[p][1])
     final_list[p].append(final_list[p][4] - final_list[p][2])
-
+        
 display_table(final_list)
 display_metrics(final_list)
