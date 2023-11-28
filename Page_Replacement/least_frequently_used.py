5da329c867eee80c0e08aa263e40c8fd7c009e7e
Hawks <126882395+h4wks123@users.noreply.github.com>
2023-11-29 02:08:42 +0800

    [BAYER][Finished algorithm logic for LFU]

--- Page_Replacement/least_frequently_used.py
+++ Page_Replacement/least_frequently_used.py
@@ -1,14 +1,165 @@
-from prettytable import PrettyTable
+#LRU page replacement algorithm 
+import copy
 
-# Example data
-referenceString = [1, 2, 3, 4]
+def LeastFrequentlyUsed(frames, referenceString):
+    arr = [] #This two dimensional array holds the dimensional array of page replacement
+    referenceStringCopy = copy.deepcopy(referenceString) #This copies the reference string for FCFS
+    pageFault = [''] * len(referenceString)  #This holds the page fault if hit or not
+    counter = [list(set(referenceString)), [0] * len(set(referenceString))] #This is a two dimensional array that counts the least frequently used value
+    pl = [''] * len(referenceString) #This one dimensional array will contain string parameters for printing the page log
+    org = 0 #ReferenceStringCopy global index
 
-# Create a PrettyTable
-table = PrettyTable()
+    for i in range(frames):
+        arr.append([-1] * len(referenceString))  # Initialize arr with -1 values
+    
+    for x in range(len(referenceString)):
+        
+        if x >= 1:
+            for y in range(frames):
+                if arr[y][x - 1] == -1:
+                    break
+                else:
+                    arr[y][x] = arr[y][x - 1]
 
-# Add columns to the table
-column_names = ["Time"] + [f"{num}" for num in referenceString]
-table.field_names = column_names
+        hit = -1
 
-# Print the table
-print(table)
+        for y in range(frames): #Checks if reference String is a hit or not
+            if referenceString[x] == arr[y][x]:
+                for z in range(len(counter[0])):
+                    if counter[0][z] == referenceString[x]:
+                        counter[1][z] += 1
+                        hit = 1
+        
+               
+        if hit == -1:
+            
+            # Get the smallest non-zero value in the specified row
+            smallest_non_zero = min((value for value in counter[1] if value != 0), default=0)
+            
+            # For loop to look for the index with the smallest number
+            referenceLib = []  # Used to hold index for FCFS or LFU
+            
+            if smallest_non_zero > 0:
+                for y in range(len(counter[0])):
+                    if counter[1][y] == smallest_non_zero:
+                        referenceLib.append(counter[0][y])
+                        
+            #If negative 1
+            if any(row[x] == -1 for row in arr):
+                for y in range(frames):
+                    if arr[y][x] == -1:
+                        arr[y][x] = referenceString[x] 
+                        break
+                for y in range(len(counter[0])):
+                    if counter[0][y] == referenceString[x]:
+                        counter[1][y] += 1
+                        
+            #If condition has duplicates
+            elif len(referenceLib) > 1:
+                
+                cop = org
+                holds = -1
+                
+                for cop in range(len(referenceStringCopy)):
+                    for lib in range(len(referenceLib)):
+                        if referenceStringCopy[cop] == referenceLib[lib]: 
+                            for y in range(frames): #Swaps value to FCFS
+                                if arr[y][x] == referenceLib[lib]:
+                                    arr[y][x] = referenceString[x]
+                                    break
+                            for y in range(len(counter[0])): #Changes val to zero
+                                if counter[0][y] == referenceLib[lib]:
+                                    counter[1][y] = 0
+                                    break
+                            for y in range(len(counter[0])): #New value becomes 1
+                                if counter[0][y] == referenceString[x]:
+                                    counter[1][y] = 1
+                                    break
+                            referenceStringCopy[cop] = -1
+                            holds = 0
+                    if holds == 0:
+                        break
+                    
+                org = cop
+   
+            #If condition has no duplicates             
+            elif len(referenceLib) == 1:
+                for y in range(frames):
+                    if arr[y][x] == referenceLib[0]:
+                        arr[y][x] = referenceString[x]
+                for z in range(len(counter[0])):
+                    if counter[0][z] == referenceLib[0]:
+                        counter[1][z] = 0
+                    if counter[0][z] == referenceString[x]:
+                        counter[1][z] += 1                        
+
+
+#7 0 1 2 0 3 0 4 2 3 0 3 2 1 2
+
+    displayFrames(arr, referenceString, frames)
+
+
+#Prints the Frame chart
+def displayFrames(arr, referenceString, frames):
+
+    print("")
+    print("[-----MEMORY STATE VISUALIZATION:-----]")
+    print("")
+
+    print("{:<14}".format("--------------------"), end="")
+
+    for i in range(len(referenceString)):
+        print("{:<4}".format("-----"), end="")
+
+    print("")
+    print("{:<21}".format("| Reference String |"), end="")
+
+    for i in range(len(referenceString)):
+        print("{:<2} {:<2}".format(referenceString[i], "|"), end="")
+
+    print("")    
+    print("{:<14}".format("--------------------"), end="")
+
+    for i in range(len(referenceString)):
+        print("{:<4}".format("-----"), end="")
+
+    print("")      
+
+    for x in range(frames):
+        print("{:<21}".format(f"| Frame #{x + 1}         |"), end="")
+        for y in range(len(referenceString)):
+            if arr[x][y] == -1:
+                print("{:<2} {:<2}".format("-", "|"), end="")
+            else:
+                print("{:<2} {:<2}".format(arr[x][y], "|"), end="")
+        print("")
+
+    print("{:<14}".format("--------------------"), end="")
+
+    for i in range(len(referenceString)):
+        print("{:<4}".format("-----"), end="")
+
+    print("")
+            
+
+
+# Get input from the user as a string
+
+print("")
+print("[-----LFU PAGE REPLACEMENT ALGORITHM-----]")
+print("")
+
+input_string = input("Enter the reference string: ")
+numbers_as_strings = input_string.split()
+referenceString = [int(num) for num in numbers_as_strings]
+
+frames = int(input("Enter the number of frames: "))
+
+LeastFrequentlyUsed(frames, referenceString)
+
+
+#Test Case 1
+#7 0 1 2 0 3 0 4 2 3 0 3 2 1 2
+
+#Test Case 2
+#5 2 1 0 2 1 3 2 1 1 3 0 3
